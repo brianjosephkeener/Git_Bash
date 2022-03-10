0e1525f6a26d3f05d9c97c369f7c139857be1800
Brian Keener <brian120496@gmail.com>
2022-03-09 22:50:38 -0500

    update

--- Script_Practice_Day2/firstscript.sh
+++ Script_Practice_Day2/firstscript.sh
@@ -4,3 +4,18 @@ mkdir ./testfolder
 touch ./testfolder/name.txt
 
 echo "Hello New File!" >> ./testfolder/name.txt
+
+#for example
+for (( i=0; i <= 5; i++ ))
+do 
+	echo $i >> ./testfolder/name.txt
+done
+
+#if example
+j=16
+if [ $j -eq 15 ];
+	then echo "true"
+else
+	echo "false"
+fi
+
